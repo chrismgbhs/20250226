0c38452f3c2f8b9f7f2984c415efda4dea907e0f
chrisboenmgbhs <chrisboen8@outlook.com>
2025-02-26 13:49:47 +0800

    Add project files.

---
+++ 20250226/Program.cs
@@ -0,0 +1,11 @@
+﻿string x = "Southville International School and Colleges";
+string[] y = x.Split(' ');
+
+for (int i = 0; i < y.Length; i++)
+{
+    for (int j = 0; j < y[i].Length; j++)
+    {
+        Console.WriteLine(y[i][j]);
+    }
+    Console.ReadKey();
+}
